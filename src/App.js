f3bff6cb2a4db5a32902f4e203a13e74817c62ad
bankvooruit <theaterbankvooruit@gmail.com>
2022-10-17 11:29:01 +0200

    Add PUBLIC_URL as a basename to the router

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
   );
 
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <div className="h-100">
         <div className="grid grid-flow-row auto-rows-max z-20">
           <TopMenu />
