14d5b5b274e1fed4bfb89bd26ca8ec6a8c02c20f
bankvooruit <theaterbankvooruit@gmail.com>
2026-02-17 22:46:48 +0100

    QUIZ - Voorbij

--- src/gegevens/2025-quiz.js
+++ src/gegevens/2025-quiz.js
@@ -1,55 +1,54 @@
 export const quiz_id = "2025-quiz";
 
-const link_inschrijvingsformulier =
-  "https://docs.google.com/forms/d/1MpdZU9_tBPZEZFvIfY0y6aeOuanlztii-qL51a0PaCo/viewform?edit_requested=true";
-const korte_inhoud =
-  "Een lichtvoetige familiequiz voor iedereen die al eens van quizzen houdt. In samenwerking met Theaterling, ten voordele van de MS-Liga Vlaanderen";
-// const terugblik =
-//    "In het najaar van 2023 hielden we een te gekke en zeer gesmaakte Bank Vooruit-quiz in Lille. Bedankt aan alle ploegen die toen aanwezig waren. Dankzij jullie kunnen we in 2024 'De legende van Roloc' maken en alle ticketopbrengsten wegschenken. Dikke merci en wie weet... tot een volgende Quiz!",
-
+// const link_inschrijvingsformulier =
+//   "https://docs.google.com/forms/d/1MpdZU9_tBPZEZFvIfY0y6aeOuanlztii-qL51a0PaCo/viewform?edit_requested=true";
+// const korte_inhoud =
+//   "Een lichtvoetige familiequiz voor iedereen die al eens van quizzen houdt. In samenwerking met Theaterling, ten voordele van de MS-Liga Vlaanderen";
+const terugblik =
+  "In het najaar van 2025 hielden we een te gekke en zeer gesmaakte Bank Vooruit-quiz in Herentals, in samenwerking met Theaterling. Bedankt aan alle ploegen die toen aanwezig waren. Dankzij jullie kunnen we in 2026 een nieuwe voorstelling maken en alle ticketopbrengsten wegschenken. Dikke merci en wie weet... tot een volgende Quiz!";
 
 export const quiz = {
   jaartal: "2025",
-  link_inschrijvingsformulier,
-  flyer: "quizFlyer.png",
-  korte_inhoud,
-  // terugblik,
-  eigenschappen: [
-    {
-      label: "Wat",
-      value: korte_inhoud,
-    },
-    {
-      label: "Wanneer",
-      value: [
-        "Zaterdag 29 november 2025",
-        "Deuren openen 19u, eerste vraag om 19u30",
-      ],
-    },
-    {
-      label: "Waar",
-      value: ["Zaal 't Hof", "Grote Markt 41", "2200 Herentals"],
-    },
-    {
-      label: "Ploegen",
-      value: "Ploegen van maximaal 5 personen",
-    },
-    {
-      label: "Inschrijvingsgeld",
-      value: "€25 per ploeg",
-    },
-    {
-      label: "Inschrijven",
-      value: [
-        {
-          label: "Vul het inschrijfformulier in!",
-          link: link_inschrijvingsformulier,
-        },
-        'Je inschrijving is pas geldig na ontvangst van het inschrijvingsgeld op rekeningnummer: BE93 9734 1677 3567 met de vermelding: "ploegnaam + TBV Quiz"',
-        "Indien u zich wenst uit te schrijven, stuurt u een mail naar theaterbankvooruit@gmail.com",
-      ],
-    },
-  ],
+  // link_inschrijvingsformulier,
+  // flyer: "quizFlyer.png",
+  // korte_inhoud,
+  terugblik,
+  // eigenschappen: [
+  //   {
+  //     label: "Wat",
+  //     value: korte_inhoud,
+  //   },
+  //   {
+  //     label: "Wanneer",
+  //     value: [
+  //       "Zaterdag 29 november 2025",
+  //       "Deuren openen 19u, eerste vraag om 19u30",
+  //     ],
+  //   },
+  //   {
+  //     label: "Waar",
+  //     value: ["Zaal 't Hof", "Grote Markt 41", "2200 Herentals"],
+  //   },
+  //   {
+  //     label: "Ploegen",
+  //     value: "Ploegen van maximaal 5 personen",
+  //   },
+  //   {
+  //     label: "Inschrijvingsgeld",
+  //     value: "€25 per ploeg",
+  //   },
+  //   {
+  //     label: "Inschrijven",
+  //     value: [
+  //       {
+  //         label: "Vul het inschrijfformulier in!",
+  //         link: link_inschrijvingsformulier,
+  //       },
+  //       'Je inschrijving is pas geldig na ontvangst van het inschrijvingsgeld op rekeningnummer: BE93 9734 1677 3567 met de vermelding: "ploegnaam + TBV Quiz"',
+  //       "Indien u zich wenst uit te schrijven, stuurt u een mail naar theaterbankvooruit@gmail.com",
+  //     ],
+  //   },
+  // ],
 };
 
 export const sponsors = {
